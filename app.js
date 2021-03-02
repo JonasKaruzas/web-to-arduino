dbb25b7ebf59e6e83ebf67041319ae88c7868a59
Jonas Karuzas <jonas.karuzas@gmail.com>
2021-03-02 22:24:58 +0200

    working

---
+++ app.js
@@ -0,0 +1,53 @@
+var http = require('http');
+var fs = require('fs');
+
+var index = fs.readFileSync('index.html');
+
+
+var SerialPort = require('serialport');
+
+const parsers = SerialPort.parsers;
+const parser = new parsers.Readline({
+    delimiter: '\r\n'
+});
+
+var port = new SerialPort('COM8', {
+    boudRate: 9600,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false
+});
+
+port.pipe(parser);
+
+
+
+setTimeout(function() {
+    console.log('ON');
+    port.write('1');
+}, 3000);
+
+var app = http.createServer(function(req, res) {
+
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.end(index);
+
+
+});
+
+var io = require('socket.io')(app);
+
+io.on('connection', function(socket) {
+    socket.on('lights', function(data){
+        port.write(data.status);
+        console.log(data);
+        console.log(data.status);
+    })
+})
+
+
+
+app.listen(3000);
+
+
